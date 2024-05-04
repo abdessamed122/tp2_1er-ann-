03ae10171c17741bf8e1ac7f0ccf3b93556db9c4
abdessamed122 <159251046+abdessamed122@users.noreply.github.com>
2024-05-04 22:26:54 +0200

    Update tp_1er.c

--- programe tp 1 er/tp_1er.c
+++ programe tp 1 er/tp_1er.c
@@ -10,103 +10,103 @@ typedef struct {
     char numtlf[10];
     char motdepass[20];
     float solde;
-} compte;
+} cart;
 
-compte tab_compte[100];
-int nb_compte = 0;
+cart tab_cart[100];
+int nb_cart = 0;
 
-void sauvegarder_comptes() {
+void sauvegarder_carts() {
     FILE *f;
     int i;
 
-    f = fopen("comptes.txt", "w");
+    f = fopen("carts.txt", "w");
 
     if (f == NULL) {
         printf("Erreur d'ouverture du fichier.\n");
         return;
     }
-    for (i = 0; i < nb_compte; i++) {
-        fprintf(f, "%s %s %s %.2f %s %s %s\n" ,tab_compte[i].nom, tab_compte[i].prenom, tab_compte[i].num, tab_compte[i].motdepass,&tab_compte[i].solde ,tab_compte[i].adress,tab_compte[i].numtlf);
+    for (i = 0; i < nb_cart; i++) {
+        fprintf(f, "%s %s %s %.2f %s %s %s\n" ,tab_cart[i].nom, tab_cart[i].prenom, tab_cart[i].num, tab_cart[i].motdepass,&tab_cart[i].solde ,tab_cart[i].adress,tab_cart[i].numtlf);
     }
 
     fclose(f);
 }
 
-void charger_comptes() {
+void charger_carts() {
     FILE *f;
     char ligne[50];
     int i = 0;
 
-    f = fopen("comptes.txt", "r");
+    f = fopen("carts.txt", "r");
 
 
     if (f == NULL) {
-        printf("Aucun compte enregistre.\n");
+        printf("Aucune cart enregistre.\n");
         return;
     }
 
     while (fgets(ligne, 50, f) != NULL) {
-        sscanf(ligne, "%s %s %s %s %s %s%s",tab_compte[i].nom, tab_compte[i].prenom, tab_compte[i].num, tab_compte[i].motdepass,&tab_compte[i].solde ,tab_compte[i].adress,tab_compte[i].numtlf);
+        sscanf(ligne, "%s %s %s %s %s %s%s",tab_cart[i].nom, tab_cart[i].prenom, tab_cart[i].num, tab_cart[i].motdepass,&tab_cart[i].solde ,tab_cart[i].adress,tab_cart[i].numtlf);
         i++;
     }
 
-    nb_compte = i;
+    nb_cart = i;
 
     fclose(f);
 }
 
-int creation_compte() {
-    if (nb_compte >= 100) {
-        printf("Nombre maximum de comptes atteint.\n");
+int creation_cart() {
+    if (nb_cart >= 100) {
+        printf("Nombre maximum de carts atteint.\n");
         return 0;
     }
  printf("Nom : ");
-    scanf("%s", tab_compte[nb_compte].nom);
+    scanf("%s", tab_cart[nb_cart].nom);
 
     printf("Prenom : ");
-    scanf("%s", tab_compte[nb_compte].prenom);
+    scanf("%s", tab_cart[nb_cart].prenom);
 
-    printf("Numero de compte : ");
-    scanf("%s", tab_compte[nb_compte].num);
+    printf("Numero de cart : ");
+    scanf("%s", tab_cart[nb_cart].num);
    printf("mot de pass : ");
-    scanf("%s", tab_compte[nb_compte].motdepass);
+    scanf("%s", tab_cart[nb_cart].motdepass);
    
     printf("Solde initial : ");
-    scanf("%f", &tab_compte[nb_compte].solde);
+    scanf("%f", &tab_cart[nb_cart].solde);
  
      printf("adress : ");
-    scanf("%s", tab_compte[nb_compte].adress);
+    scanf("%s", tab_cart[nb_cart].adress);
      printf("numero de tlephone : ");
-    scanf("%s", tab_compte[nb_compte].numtlf);
+    scanf("%s", tab_cart[nb_cart].numtlf);
 
-    nb_compte++;
+    nb_cart++;
 
-    sauvegarder_comptes();
+    sauvegarder_carts();
 
-    printf("Compte cree avec succes.\n");
+    printf("cart cree avec succes.\n");
 
     return 1;
 }
 
-int connexion_compte() {
+int connexion_cart() {
     char num[10];
     char mot_de_passe[20];
     int i;
 
-    printf("Numero de compte : ");
+    printf("Numero de cart : ");
     scanf("%s", num);
 
     printf("Mot de passe : ");
     scanf("%s", mot_de_passe);
 
-    for (i = 0; i < nb_compte; i++) {
-        if (strcmp(num, tab_compte[i].num) == 0) {
+    for (i = 0; i < nb_cart; i++) {
+        if (strcmp(num, tab_cart[i].num) == 0) {
             return i;
         }
     }
     
 
-    printf("Compte introuvable.\n");
+    printf("cart introuvable.\n");
     return -1;
 }
 
@@ -117,11 +117,11 @@ void depot() {
     printf("Montant du depot : ");
     scanf("%f", &montant);
 
-      i = connexion_compte();
+      i = connexion_cart();
 
     if (i != -1) {
-        tab_compte[i].solde += montant;
-        sauvegarder_comptes();
+        tab_cart[i].solde += montant;
+        sauvegarder_carts();
         printf("Depot effectue avec succes.\n");
     }
 }
@@ -135,7 +135,7 @@ int compare(const void *a, const void *b) {
 }
 
 void tri() {
-    FILE *file = fopen("comptes.txt", "r");
+    FILE *file = fopen("carts.txt", "r");
     if (file == NULL) {
         perror("Error opening file");
         return 1;
@@ -167,7 +167,7 @@ void tri() {
     qsort(lines, count, sizeof(char *), compare);
 
     // Write the sorted lines back to the file
-    file = fopen("comptes.txt", "w");
+    file = fopen("carts.txt", "w");
     if (file == NULL) {
         perror("Error opening file");
         free(lines);
@@ -191,12 +191,12 @@ void retrait() {
     printf("Montant du retrait : ");
     scanf("%f", &montant);
 
-    i = connexion_compte();
+    i = connexion_cart();
 
     if (i != -1) {
-        if (tab_compte[i].solde >= montant) {
-            tab_compte[i].solde -= montant;
-            sauvegarder_comptes();
+        if (tab_cart[i].solde >= montant) {
+            tab_cart[i].solde -= montant;
+            sauvegarder_carts();
             printf("Retrait effectue avec succes.\n");
         } else {
             printf("Solde insuffisant.\n");
@@ -207,94 +207,94 @@ void retrait() {
 void solde() {
     int i;
 
-    i = connexion_compte();
+    i = connexion_cart();
 
     if (i != -1) {
-        printf("%s %s\n", tab_compte[i].nom, tab_compte[i].prenom);
-        printf("Solde : %.2f\n", tab_compte[i].solde);
+        printf("%s %s\n", tab_cart[i].nom, tab_cart[i].prenom);
+        printf("Solde : %.2f\n", tab_cart[i].solde);
     }
 }
 int reche () {
     char num[10];
     int i;
 
-    printf("Numero de compte : ");
+    printf("Numero de cart : ");
     scanf("%s", num);
 
-    for (i = 0; i < nb_compte; i++) {
-       if (strcmp(num, tab_compte[i].num) == 0) {
-            printf("Compte trouve : \n");
-            printf("Nom : %s\n", tab_compte[i].nom);
-            printf("Prenom : %s\n", tab_compte[i].prenom);
-            printf("Numero de compte : %s\n", tab_compte[i].num);
-            printf("Mot de passe : %s\n", tab_compte[i].motdepass);
-            printf("Solde : %.2f\n", tab_compte[i].solde);
-            printf("Adresse : %s\n", tab_compte[i].adress);
-            printf("Numero de telephone : %s\n", tab_compte[i].numtlf);
+    for (i = 0; i < nb_cart; i++) {
+       if (strcmp(num, tab_cart[i].num) == 0) {
+            printf("cart trouve : \n");
+            printf("Nom : %s\n", tab_cart[i].nom);
+            printf("Prenom : %s\n", tab_cart[i].prenom);
+            printf("Numero de cart : %s\n", tab_cart[i].num);
+            printf("Mot de passe : %s\n", tab_cart[i].motdepass);
+            printf("Solde : %.2f\n", tab_cart[i].solde);
+            printf("Adresse : %s\n", tab_cart[i].adress);
+            printf("Numero de telephone : %s\n", tab_cart[i].numtlf);
             return i;
         }
     }
-   printf("Compte introuvable.\n");
+   printf("cart introuvable.\n");
     return -1;
 }
-void supprimer_compte(int index) {
-    if (index >= 0 && index < nb_compte) {
-        printf("Compte supprime : \n");
-        printf("Nom : %s\n", tab_compte[index].nom);
-        printf("Prenom : %s\n", tab_compte[index].prenom);
-        printf("Numero de compte : %s\n", tab_compte[index].num);
-        printf("Mot de passe : %s\n", tab_compte[index].motdepass);
-        printf("Solde : %.2f\n", tab_compte[index].solde);
-        printf("Adresse : %s\n", tab_compte[index].adress);
-        printf("Numero de telephone : %s\n", tab_compte[index].numtlf);
+void supprimer_cart(int index) {
+    if (index >= 0 && index < nb_cart) {
+        printf("cart supprime : \n");
+        printf("Nom : %s\n", tab_cart[index].nom);
+        printf("Prenom : %s\n", tab_cart[index].prenom);
+        printf("Numero de cart : %s\n", tab_cart[index].num);
+        printf("Mot de passe : %s\n", tab_cart[index].motdepass);
+        printf("Solde : %.2f\n", tab_cart[index].solde);
+        printf("Adresse : %s\n", tab_cart[index].adress);
+        printf("Numero de telephone : %s\n", tab_cart[index].numtlf);
 
-        for (int i = index; i < nb_compte - 1; i++) {
-            tab_compte[i] = tab_compte[i+1];
+        for (int i = index; i < nb_cart - 1; i++) {
+            tab_cart[i] = tab_cart[i+1];
         }
-        nb_compte--;
-        sauvegarder_comptes();
-        printf("Compte supprime avec succes.\n");
+        nb_cart--;
+        sauvegarder_carts();
+        printf("cart supprime avec succes.\n");
     } else {
-        printf("Compte introuvable.\n");
+        printf("cart introuvable.\n");
     }
 }
-void modifier_compte(int index) {
-    if (index >= 0 && index < nb_compte) {
-        printf("Entrez les nouvelles valeurs pour le compte %s %s:\n", tab_compte[index].nom, tab_compte[index].prenom);
+void modifier_cart(int index) {
+    if (index >= 0 && index < nb_cart) {
+        printf("Entrez les nouvelles valeurs pour le cart %s %s:\n", tab_cart[index].nom, tab_cart[index].prenom);
         printf("Nom : ");
-        scanf("%s", tab_compte[index].nom);
+        scanf("%s", tab_cart[index].nom);
         printf("Prenom : ");
-        scanf("%s", tab_compte[index].prenom);
-        printf("Numero de compte : ");
-        scanf("%s", tab_compte[index].num);
+        scanf("%s", tab_cart[index].prenom);
+        printf("Numero de cart : ");
+        scanf("%s", tab_cart[index].num);
         printf("Mot de passe : ");
-        scanf("%s", tab_compte[index].motdepass);
+        scanf("%s", tab_cart[index].motdepass);
         printf("Solde initial : ");
-        scanf("%f", &tab_compte[index].solde);
+        scanf("%f", &tab_cart[index].solde);
         printf("Adresse : ");
-        scanf("%s", tab_compte[index].adress);
+        scanf("%s", tab_cart[index].adress);
         printf("Numero de telephone : ");
-        scanf("%s", tab_compte[index].numtlf);
-        sauvegarder_comptes();
-        printf("Compte modifie avec succes.\n");
+        scanf("%s", tab_cart[index].numtlf);
+        sauvegarder_carts();
+        printf("cart modifie avec succes.\n");
     } else {
-        printf("Compte introuvable.\n");
+        printf("cart introuvable.\n");
     }
 }
-void inserer_compte_triee() {
-    FILE *fichier = fopen("comptes.txt", "r+");
+void inserer_cart_triee() {
+    FILE *fichier = fopen("carts.txt", "r+");
     if (fichier == NULL) {
         printf("Erreur d'ouverture du fichier.\n");
         return;
     }
 
     // Lire tous les éléments du fichier dans un tableau en mémoire
-    compte tab_comptes[100];
-    int nb_comptes = 0;
-    compte c;
+    cart tab_carts[100];
+    int nb_carts = 0;
+    cart c;
     while (fscanf(fichier, "%s %s %s %s %s %f %s\n", c.nom, c.prenom, c.num, c.motdepass, &c.solde, c.adress, c.numtlf) != EOF) {
-        tab_comptes[nb_comptes] = c;
-        nb_comptes++;
+        tab_carts[nb_carts] = c;
+        nb_carts++;
     }
 
     // Trouver l'emplacement approprié pour insérer le nouvel élément
@@ -302,22 +302,22 @@ void inserer_compte_triee() {
     scanf("%s", c.nom);
     printf("Prenom : ");
     scanf("%s", c.prenom);
-    for (int i = 0; i < nb_comptes; i++) {
-        if (strcmp(tab_comptes[i].nom, c.nom) > 0 || (strcmp(tab_comptes[i].nom, c.nom) == 0 && strcmp(tab_comptes[i].prenom, c.prenom) > 0)) {
+    for (int i = 0; i < nb_carts; i++) {
+        if (strcmp(tab_carts[i].nom, c.nom) > 0 || (strcmp(tab_carts[i].nom, c.nom) == 0 && strcmp(tab_carts[i].prenom, c.prenom) > 0)) {
             // Insérer le nouvel élément à l'emplacement approprié
-            for (int j = nb_comptes; j > i; j--) {
-                tab_comptes[j] = tab_comptes[j-1];
+            for (int j = nb_carts; j > i; j--) {
+                tab_carts[j] = tab_carts[j-1];
             }
-            tab_comptes[i] = c;
-            nb_comptes++;
+            tab_carts[i] = c;
+            nb_carts++;
             break;
         }
     }
 
     // Réécrire tout le tableau dans le fichier
     rewind(fichier);
-    for (int i = 0; i < nb_comptes; i++) {
-        fprintf(fichier, "%s %s %s %s %s %f %s\n", tab_comptes[i].nom, tab_comptes[i].prenom, tab_comptes[i].num, tab_comptes[i].motdepass, &tab_comptes[i].solde, tab_comptes[i].adress, tab_comptes[i].numtlf);
+    for (int i = 0; i < nb_carts; i++) {
+        fprintf(fichier, "%s %s %s %s %s %f %s\n", tab_carts[i].nom, tab_carts[i].prenom, tab_carts[i].num, tab_carts[i].motdepass, &tab_carts[i].solde, tab_carts[i].adress, tab_carts[i].numtlf);
     }
 
     fclose(fichier);
@@ -326,17 +326,17 @@ void inserer_compte_triee() {
 int main() {
     int choix,comsup,commodif;
    
-    charger_comptes();
+    charger_carts();
 
     while (1) {
-        printf("1- Creation de compte\n");
+        printf("1- Creation de cart\n");
         printf("2- Depot\n");
         printf("3- Retrait\n");
         printf("4- Solde\n");
         printf("5- tri par nom :\n");
-        printf("6- recherche un compte par numero d compte \n");
-        printf("7- supprime un compte par index \n");
-        printf("8- modifier un compte par index\n");
+        printf("6- recherche une cart par numero d cart \n");
+        printf("7- supprime une cart par index \n");
+        printf("8- modifier une cart par index\n");
         printf("9- Insérer un nouvel élément dans sa position (tri) selon le nom \n");
         printf("911- Quitter\n");
         printf("Votre choix : ");
@@ -344,10 +344,10 @@ int main() {
 
         switch (choix) {
             case 1:
-                creation_compte();
+                creation_cart();
                 break;
             case 22:
-                connexion_compte();
+                connexion_cart();
                 break;
             case 2:
                 depot();
@@ -366,15 +366,15 @@ int main() {
             break;
             case 7:         
 scanf( "%d" ,&comsup );
-                supprimer_compte(comsup);
+                supprimer_cart(comsup);
             break;
             case 8:     
             scanf( "%d" ,&commodif);
     
-                modifier_compte(commodif);             
+                modifier_cart(commodif);             
             break;
             case 9:         
-                inserer_compte_triee();             
+                inserer_cart_triee();             
             break;
                 case 911:
                 exit(0);
